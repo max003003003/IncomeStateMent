fc29e594364b5365db3661feb1234cdb0ef6f086
vorabhol chuaybhan <max003003003@gmail.com>
2017-02-23 00:52:17 +0700

    New Activity Edit

---
+++ app/src/main/java/com/max/incomestatement/EditWalletActivity.java
@@ -0,0 +1,101 @@
+package com.max.incomestatement;
+import com.max.incomestatement.data.WalletDbHelper;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.max.incomestatement.data.WalletContract;
+
+public class EditWalletActivity extends AppCompatActivity {
+   private Spinner spinner;
+   private  EditText name;
+    private  EditText balance;
+    private  ArrayAdapter<CharSequence> adapter;
+    private  String itemvalue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_wallet);
+        spinner = (Spinner)findViewById(R.id.spinner);
+        name = (EditText) findViewById(R.id.nameIn);
+        balance= (EditText)findViewById(R.id.balanceIn);
+        setSpinner();
+    }
+
+    private void setSpinner()
+    {
+        adapter = ArrayAdapter.createFromResource(this,R.array.array_wallter_option,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                itemvalue= adapterView.getItemAtPosition(i).toString() ;
+                
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+    private void insertWallet(){
+        String  n=name.getText().toString().trim();
+        Double  b= Double.parseDouble( balance.getText().toString().trim());
+        WalletDbHelper mDbHelper = new WalletDbHelper(this);
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(WalletContract.WalletEntry.COLUMN_WALLET_NAME, n);
+        values.put(WalletContract.WalletEntry.COLUMN_WALLET_BALANCE, b);
+        values.put(WalletContract.WalletEntry.COLUMN_WALLET_ICON,"ic_account_balance_wallet_white_24dp");
+        values.put(WalletContract.WalletEntry.COLUMN_WALLET_CURRENCY,"th");
+        long newRowID = db.insert(WalletContract.WalletEntry.TABLE_NAME,null,values);
+
+        if(newRowID == -1 ){
+            Toast.makeText(this,"error",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this,"Wallet Save"+newRowID, Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.edit_wallet, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                 insertWallet();
+                 finish();
+                return true;
+
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/max/incomestatement/MainActivity.java
+++ app/src/main/java/com/max/incomestatement/MainActivity.java
@@ -1,15 +1,27 @@
 package com.max.incomestatement;
 
+import com.max.incomestatement.data.WalletContract.WalletEntry;
+
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import com.max.incomestatement.data.WalletDbHelper;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,16 +30,26 @@ public class MainActivity extends AppCompatActivity {
     private List<Wallet> walletlist = new ArrayList<>();
     private RecyclerView recyclerView;
     private WalletAdapter wAdapter;
-    DatabaseHelper walletDB;
 
+    DatabaseHelper walletDB;
+    private WalletDbHelper mDbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         walletDB = new DatabaseHelper(this);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,EditWalletActivity.class);
+                startActivity(intent);
 
-
+            }
+        });
+        mDbHelper = new WalletDbHelper(this);
+        displayDatabaseInfo();
 
 
     }
@@ -38,5 +60,54 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void displayDatabaseInfo() {
+        WalletDbHelper mDbHelper = new WalletDbHelper(this);
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery(" SELECT * FROM " + WalletEntry.TABLE_NAME, null);
+        try{
+            TextView tvdb= (TextView) findViewById(R.id.textDB);
+            tvdb.setText("NUMBER OF ROW in"+cursor.getCount());
+        } finally {
+            cursor.close();
+        }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         getMenuInflater().inflate(R.menu.menu_wallet, menu);
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        displayDatabaseInfo();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_insert_dummy_data:
+                insertWallet();
+                displayDatabaseInfo();
+                return true;
+            case R.id.action_delete_dummy_data:
+                deleteWallet();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void insertWallet(){
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(WalletEntry.COLUMN_WALLET_NAME, "Kasikorn Bank");
+        values.put(WalletEntry.COLUMN_WALLET_BALANCE, 1256.25);
+        values.put(WalletEntry.COLUMN_WALLET_ICON,"ic_account_balance_wallet_white_24dp");
+        values.put(WalletEntry.COLUMN_WALLET_CURRENCY,"th");
+        long newRowID = db.insert(WalletEntry.TABLE_NAME,null,values);
+
+    }
+    private void deleteWallet() {
+
+    }
 }
