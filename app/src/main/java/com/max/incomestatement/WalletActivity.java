95ac2e941d640f06a95b0c225dd25d18de7ef4a9
vorabhol chuaybhan <max003003003@gmail.com>
2017-03-01 02:38:18 +0700

    transaction view implement bug at getbalance come with sign

--- app/src/main/java/com/max/incomestatement/WalletActivity.java
+++ app/src/main/java/com/max/incomestatement/WalletActivity.java
@@ -1,6 +1,7 @@
 package com.max.incomestatement;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
@@ -11,21 +12,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatImageView;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.max.incomestatement.data.CategoryContract;
 import com.max.incomestatement.data.TransactionContract;
 import com.max.incomestatement.data.WalletContract;
 
-public class WalletActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+public class WalletActivity extends AppCompatActivity {
 
     AppCompatImageView iconimage;
     TextView balancetv;
-    private Uri mCurrentWalletUri;
+    public static  Uri mCurrentWalletUri;
+    private Context contex;
+    private TransactionCursorAdapter transactionCursorAdapter;
+    private ListView transactionListView;
+    private long walletid;
 
     private static final int EXISTING_WALLET_LOADER =0;
+    private static final int EXISTING_TRANSACTION_LOADER=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -33,96 +41,105 @@ public class WalletActivity extends AppCompatActivity implements LoaderManager.L
         setContentView(R.layout.activity_wallet);
         Intent intent= getIntent();
         mCurrentWalletUri = intent.getData();
+        contex=this;
+        walletid =getIntent().getExtras().getLong("walletid");
 
        iconimage =(AppCompatImageView) findViewById(R.id.iconwallet);
+       transactionListView=(ListView) findViewById(R.id.transaction_listview);
        balancetv=(TextView) findViewById(R.id.balancewallet);
 
-        getLoaderManager().initLoader(EXISTING_WALLET_LOADER,null,this);
-        setupdata();
+        transactionCursorAdapter = new TransactionCursorAdapter(this,null);
+        transactionListView.setAdapter(transactionCursorAdapter);
+
+
+        getLoaderManager().initLoader(EXISTING_WALLET_LOADER,null,new WalletLoader());
+        getLoaderManager().initLoader(EXISTING_TRANSACTION_LOADER,null,new TransactionLoader());
+
+
 
 
     }
 
-    public void setupdata(){
-        String[] projection = {
-                TransactionContract.TransactionEntry._ID,
-                TransactionContract.TransactionEntry.COLUMN_TRANSACTION_BALANCE_AFTER,
-                TransactionContract.TransactionEntry.COLUMN_TRANSACTION_BALANCE_BEFORE,
-                TransactionContract.TransactionEntry.COLUMN_TRANSACTION_PAY,
-                TransactionContract.TransactionEntry.COLUMN_TRANSACTION_WALLET_ID,
-                TransactionContract.TransactionEntry.COLUMN_TRANSACTION_CATEGORY_ID,
-                TransactionContract.TransactionEntry.COLUMN_TRANSACTION_DATETIME,
-                TransactionContract.TransactionEntry.COLUMN_TRANSACTION_ICON
-        };
 
-        Cursor cursor = getContentResolver().query(TransactionContract.TransactionEntry.CONTENT_URI,projection,null,null,null);
-        if(cursor == null){ return;}
-        String s = "";
-        while(cursor.moveToNext())
-        {
-            s+=cursor.getInt(cursor.getColumnIndex(TransactionContract.TransactionEntry._ID))+" |"
-                    +cursor.getDouble(cursor.getColumnIndex(TransactionContract.TransactionEntry.COLUMN_TRANSACTION_BALANCE_AFTER))+" |"
-                    +cursor.getDouble(cursor.getColumnIndex(TransactionContract.TransactionEntry.COLUMN_TRANSACTION_BALANCE_BEFORE))+" |"
-                    +cursor.getDouble(cursor.getColumnIndex(TransactionContract.TransactionEntry.COLUMN_TRANSACTION_PAY))+" |"
-                    +cursor.getInt(cursor.getColumnIndex(TransactionContract.TransactionEntry.COLUMN_TRANSACTION_DATETIME));
 
+    class WalletLoader implements LoaderManager.LoaderCallbacks<Cursor>{
+
+        @Override
+        public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+
+            String[] projection = {
+                    WalletContract.WalletEntry._ID,
+                    WalletContract.WalletEntry.COLUMN_WALLET_NAME,
+                    WalletContract.WalletEntry.COLUMN_WALLET_ICON,
+                    WalletContract.WalletEntry.COLUMN_WALLET_BALANCE,
+                    WalletContract.WalletEntry.COLUMN_WALLET_CURRENCY,
+            };
+            return new CursorLoader(getApplicationContext(),mCurrentWalletUri,projection,null,null,null);
         }
 
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-        alertDialogBuilder.setMessage(s);
-        alertDialogBuilder.show();
-    }
-
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-
-        String[] projection = {
-                WalletContract.WalletEntry._ID,
-                WalletContract.WalletEntry.COLUMN_WALLET_NAME,
-                WalletContract.WalletEntry.COLUMN_WALLET_ICON,
-                WalletContract.WalletEntry.COLUMN_WALLET_BALANCE,
-                WalletContract.WalletEntry.COLUMN_WALLET_CURRENCY,
-        };
-
-
-
-        return new CursorLoader(this,mCurrentWalletUri,projection,null,null,null);
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
             if(cursor==null || cursor.getCount() < 1){
                 return;
             }
-        if(cursor.moveToFirst()){
+            if(cursor.moveToFirst()){
 
-            int idColumnIndex = cursor.getColumnIndex(WalletContract.WalletEntry._ID);
-            int nameColumnIndex = cursor.getColumnIndex(WalletContract.WalletEntry.COLUMN_WALLET_NAME);
-            int balanceIndex = cursor.getColumnIndex(WalletContract.WalletEntry.COLUMN_WALLET_BALANCE);
-            int currencyIndex = cursor.getColumnIndex(WalletContract.WalletEntry.COLUMN_WALLET_CURRENCY);
-            int iconIndex = cursor.getColumnIndex(WalletContract.WalletEntry.COLUMN_WALLET_ICON);
+                int idColumnIndex = cursor.getColumnIndex(WalletContract.WalletEntry._ID);
+                int nameColumnIndex = cursor.getColumnIndex(WalletContract.WalletEntry.COLUMN_WALLET_NAME);
+                int balanceIndex = cursor.getColumnIndex(WalletContract.WalletEntry.COLUMN_WALLET_BALANCE);
+                int currencyIndex = cursor.getColumnIndex(WalletContract.WalletEntry.COLUMN_WALLET_CURRENCY);
+                int iconIndex = cursor.getColumnIndex(WalletContract.WalletEntry.COLUMN_WALLET_ICON);
 
-            String walletName=cursor.getString(nameColumnIndex);
-            Double walletBalance =cursor.getDouble(balanceIndex);
-            String WalletIcon = cursor.getString(iconIndex);
-            Wallet wallet =new Wallet(walletName,walletBalance,WalletIcon,"th");
+                String walletName=cursor.getString(nameColumnIndex);
+                Double walletBalance =cursor.getDouble(balanceIndex);
+                String WalletIcon = cursor.getString(iconIndex);
+                Wallet wallet =new Wallet(walletName,walletBalance,WalletIcon,"th");
 
-            getSupportActionBar().setTitle(wallet.getName());
+                getSupportActionBar().setTitle(wallet.getName());
 
-            balancetv.setText(wallet.getBalace());
-            iconimage.setImageResource(wallet.getIcon());
+                balancetv.setText(wallet.getBalace());
+                iconimage.setImageResource(wallet.getIcon());
 
+
+            }
 
         }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+
+            balancetv.setText("");
+            setTitle("");
+        }
+
     }
 
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-            balancetv.setText("");
-           getSupportActionBar().setTitle("");
-           iconimage.setImageResource(0);
+    class TransactionLoader implements LoaderManager.LoaderCallbacks<Cursor>{
 
+        @Override
+        public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+            String[] projection = {
+                    TransactionContract.TransactionEntry._ID,
+                    TransactionContract.TransactionEntry.COLUMN_TRANSACTION_BALANCE_AFTER,
+                    TransactionContract.TransactionEntry.COLUMN_TRANSACTION_BALANCE_BEFORE,
+                    TransactionContract.TransactionEntry.COLUMN_TRANSACTION_PAY,
+                    TransactionContract.TransactionEntry.COLUMN_TRANSACTION_WALLET_ID,
+                    TransactionContract.TransactionEntry.COLUMN_TRANSACTION_CATEGORY_NAME,
+                    TransactionContract.TransactionEntry.COLUMN_TRANSACTION_DATETIME,
+                    TransactionContract.TransactionEntry.COLUMN_TRANSACTION_ICON  };
+
+            return new CursorLoader(contex, TransactionContract.TransactionEntry.CONTENT_URI,projection, TransactionContract.TransactionEntry.COLUMN_TRANSACTION_WALLET_ID+" =  "+walletid  ,null,null);
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+            transactionCursorAdapter.swapCursor(cursor);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            transactionCursorAdapter.swapCursor(null);
+        }
     }
 
     public void addTransaction (View view){
@@ -130,10 +147,13 @@ public class WalletActivity extends AppCompatActivity implements LoaderManager.L
         intent.putExtra("mode",1);
         this.startActivity(intent);
     }
+
+
     public void withdrawTransaction (View view){
         Intent intent = new Intent(this,EditTransaction.class);
         intent.putExtra("mode",2);
-        intent.putExtra("balance",balancetv.getText().toString().trim());
+        intent.putExtra("balance", balancetv.getText().toString());
+        intent.putExtra("walletid",walletid);
         intent.setData(mCurrentWalletUri);
         this.startActivity(intent);
     }
